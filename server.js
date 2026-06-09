519fb4a2f643c70c481d219615a6c81ba5d60808
rahul-pagare <rahulpagare613@gmail.com>
2026-06-09 14:38:25 +0530

    push

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const connectDB = require("./src/db/db")
 require("dotenv").config()
 connectDB()
 
-app.listen(3000 , ()=>{
-    console.log("sever running on port 3000");
-    
-})
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
