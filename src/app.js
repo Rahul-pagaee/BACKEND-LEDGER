3196a9b58942dd6f3666a58212167535c3818047
rahul-pagare <rahulpagare613@gmail.com>
2026-06-09 14:14:45 +0530

    Add root route to log server status

--- src/app.js
+++ src/app.js
@@ -14,6 +14,10 @@ const transactionRoutes = require("./routes/transaction.route");
 /**
  * - Use Routers
  */
+app.get("/",(req,res)=>{
+    console.log("Ledger Server is up and runnig")
+})
+
 app.use("/api/auth" , authRouter)    
 app.use("/api/accounts",accountRouter)
 app.use("/api/transactions", transactionRoutes)
